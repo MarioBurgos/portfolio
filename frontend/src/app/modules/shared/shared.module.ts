8ac589818a51568d217e9cd4ca28860abeffbee3
Mario <mario.burpi@gmail.com>
2022-03-21 12:10:57 +0100

    añado componente curtains

--- frontend/src/app/modules/shared/shared.module.ts
+++ frontend/src/app/modules/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarouselComponent } from './carousel/carousel.component';
+import { CurtainsComponent } from './curtains/curtains.component';
 
 
 
 @NgModule({
   declarations: [
-    CarouselComponent
+    CarouselComponent,
+    CurtainsComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    CarouselComponent
+    CarouselComponent,
+    CurtainsComponent
   ]
 })
 export class SharedModule { }
